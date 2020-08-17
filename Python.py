ad878a3f35c2315a6faee0917112a2d2bfa1d3f5
mrbsharma <69790941+mrbsharma@users.noreply.github.com>
2020-08-17 15:11:30 +0530

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,10 @@
+import sys
+sys.stdin = open('input.txt', 'r')
+sys.stdout = open('output.txt', 'w')
+
+a=input()
+b=input()
+print(a,"is studying in",b)
+listofcosmetic =["soap","powder","shampoo"]
+print(listofcosmetic)
+
